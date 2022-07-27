8ce3d2142d2c610f28e9f1b0409800272ad0753f
Rory Mulhern <rmulhern1998@live.com>
2022-07-27 15:59:08 +0100

    Initial Hologram Set-up

--- Assets/Scripts/NPCAnimationController.cs
+++ Assets/Scripts/NPCAnimationController.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class NPCAnimationController : MonoBehaviour
 {
-    [SerializeField] Animator animator;
+    [SerializeField] GameObject npcAnimator;
     [SerializeField] GameObject[] hologramSlides;
+
+    [SerializeField] int currentSlide;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSlide = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            Debug.Log("A button pressed");
+            currentSlide += 1;
+            hologramSlides[currentSlide - 1].SetActive(false);
+            hologramSlides[currentSlide].SetActive(true);
+
+            if (currentSlide > 3) {
+                hologramSlides[currentSlide].SetActive(false);
+                npcAnimator.GetComponent<Animator>().SetTrigger("AfterHologram");
+            }
+        }
     }
 
 
